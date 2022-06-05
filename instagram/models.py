0e7b926746e9c003f294aa7b6864201ec79e82b4
Charity-Mutembei <charity.mutembei@student.moringaschool.com>
2022-06-05 20:34:42 +0300

    Doc: Models added, postgres connected and migrations made

--- instagram/models.py
+++ instagram/models.py
@@ -2,15 +2,28 @@ from distutils.command.upload import upload
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+class Editor(models.Model):
+    first_name = models.CharField(max_length = 30)
+    last_name = models.CharField(max_length = 30)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length = 10, blank = True)
 
 
-class Post(models.Model):
-    location = models.CharField(max_length= 100)
-    post = models.ImageField(upload_to = 'media/posts', blank = False)
-    tags = models.ManyToManyField(tags)
+    def __str__(self):
+        return self.first_name
 
+    def save_editor(self):
+        self.save()
+    class Meta:
+        ordering = ['first_name']
 class tags(models.Model):
     name = models.CharField(max_length = 30)
 
     def __str__ (self):
         return self.name
+class Post(models.Model):
+    location = models.CharField(max_length= 100)
+    post = models.ImageField(upload_to = 'media/posts', blank = False)
+    tags = models.ManyToManyField(tags)
+    editor = models.ForeignKey(User,on_delete=models.CASCADE)
+
