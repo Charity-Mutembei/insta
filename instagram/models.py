729a228b42153ed9fbc07b0affd66dac788c4dd0
Charity-Mutembei <charity.mutembei@student.moringaschool.com>
2022-06-05 17:38:38 +0300

    Doc: post and tags model classes created

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,16 @@
+from distutils.command.upload import upload
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Post(models.Model):
+    location = models.CharField(max_length= 100)
+    post = models.ImageField(upload_to = 'media/posts', blank = False)
+    tags = models.ManyToManyField(tags)
+
+class tags(models.Model):
+    name = models.CharField(max_length = 30)
+
+    def __str__ (self):
+        return self.name
