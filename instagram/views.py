d89011d90cd098d8c219002720a6c61499246bf1
Charity-Mutembei <charity.mutembei@student.moringaschool.com>
2022-06-05 22:06:19 +0300

    Doc: views.py updated with the forms, and editor functions

--- instagram/views.py
+++ instagram/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from .forms import NewPostForm
+from django.shortcuts import render, redirect
 # Create your views here.
 
 def welcome (request):
@@ -12,3 +14,18 @@ def register(request):
 
 def login (request):
     return render(request, 'registration/login.html')
+
+@login_required(login_url='/login/')
+def new_post(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.editor = current_user
+            post.save()
+        return redirect(welcome)
+    else:
+        form = NewPostForm()
+    return render(request, 'new_post.html', {'form': form})
+    
