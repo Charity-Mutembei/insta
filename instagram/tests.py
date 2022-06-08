d286893f9d611a1f9d215ec2e57441dd88ce9e4d
Charity-Mutembei <charity.mutembei@student.moringaschool.com>
2022-06-08 11:56:38 +0300

    Doc: tests created and added

--- instagram/tests.py
+++ instagram/tests.py
@@ -1,11 +1,25 @@
 from django.test import TestCase
+from .models import Post, Profile, tags, Follow, Stream
 
-# Create your tests here.
-def get_middle(s):
-    x = len(s)
-    y = int(x/2)
-    if x%2 == 0:
-        print (s[y-1:y+1])
-    else:
-        print( s[y:y+1])
-get_middle('boy')
+#create tests for the model classes
+
+class PostTestClass(TestCase):
+    """
+    this test class is one to test the class model Post
+    """
+    #setupmethod
+    def setUp(self):
+        self.post_text = Post(post_text= '')
+
+    #testing whether we are being instanciated properly
+
+    def test_instance (self):
+        self.assertTrue(isinstance(self.post_text, Post))
+
+
+    #testing the save method
+
+    def test_save_method(self):
+        self.post_text.save_post()
+        posts = Post.objects.all()
+        self.assertTrue(len(posts)>0)
